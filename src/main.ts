d7ec4ccc9a28bb91fad864e411ee53bc8e188e16
Pratikchhya Shrestha <pat@tilde.team>
2022-06-23 17:36:10 -0500

    rename asset to public

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,5 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <h1>Hello Syangja!</h1>
-  <img src="assets/biruwa.jpeg" style="max-width: 100%;max-height: 100%;" alt="biruwa, syangja" />
+  <img src="/biruwa.jpeg" style="max-width: 100%;max-height: 100%;" alt="biruwa, syangja" />
 `
