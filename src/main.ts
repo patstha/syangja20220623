bd68f03af832ac86ec385ec1bcdd0d9a8f9392b8
Pratikchhya Shrestha <pat@tilde.team>
2022-06-23 17:31:05 -0500

    add a photo of biruwa, syangja
    from https://en.wikipedia.org/wiki/Syangja_District#/media/File:Biruwa,_Gabudanda.jpg

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import './style.css'
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>Hello Syangja!</h1>
+  <img src="assets/biruwa.jpeg" style="max-width: 100%;max-height: 100%;" alt="biruwa, syangja" />
 `
