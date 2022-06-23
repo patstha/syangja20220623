09ed89d8776ec9c3f95ab19cf473f0fa7bd4a7df
Pratikchhya Shrestha <pat@tilde.team>
2022-06-23 18:16:32 -0500

    add my name

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <h1>Hello Syangja!</h1>
+  <p class="text-success">my name is pratikchhya</p>
   <img src="/biruwa.jpeg" style="max-width: 100%;max-height: 100%;" alt="biruwa, syangja" />
 `
